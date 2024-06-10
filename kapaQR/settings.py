0ab4909b4f14f2e18ce008dfcc3be8bc36753a35
M.Mehdi Hosseini <m.m.hosseini1099@gmail.com>
2024-06-10 19:46:28 +0330

    add STATICFILES_DIRS

--- kapaQR/settings.py
+++ kapaQR/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 #     os.path.join(BASE_DIR, 'static')
 # ]
 STATIC_URL = 'staticfiles/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'staticfiles')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
 STATIC_ROOT = os.path.join(BASE_DIR)
 
 MEDIA_URL = '/images/'
