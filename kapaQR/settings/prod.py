340b0928f188567a1a812507a6092d226b7cfa0e
M.Mehdi Hosseini <m.m.hosseini1099@gmail.com>
2024-06-20 13:13:01 +0330

    DATABASES

--- kapaQR/settings/prod.py
+++ kapaQR/settings/prod.py
@@ -9,3 +9,10 @@ DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['kapa.liara.run']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
